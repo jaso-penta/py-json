dbfeb74ce6526d7db0b5aa0ae5257c797d85365a
Jašo <andrija.pericic@gmail.com>
2025-10-03 20:56:49 +0200

    Update py_json_02.py

--- py_json_02.py
+++ py_json_02.py
@@ -1,7 +1,8 @@
 import json
 
 
-person = {
+contacts = [
+    {
     'first_name': 'Pero',
     'last_name': 'Peric',
     'phone': '+385 91 5887722',
@@ -13,13 +14,25 @@ person = {
         'country': 'Hrvatska'
     },
     'age': 35
+},
+    {
+    'first_name': 'Jure',
+    'last_name': 'Juro',
+    'phone': '+385 91 4567722',
+    'email': 'pero@gmail.com',
+    'address':{
+        'street': 'Ulica 111',
+        'postal_code': '23336',
+        'city': 'Zagreb',
+        'country': 'Hrvatska'
+    },
+    'age': 25
 }
-
+]
 
 try:
-    with open('person.json', 'w') as file_writer:
-        json.dump(person, file_writer, indent=4)
-        
+    with open('contacts.json', 'w') as file_writer:
+        json.dump(contacts, file_writer, indent=4)
 
 except Exception as ex:
     print(f'Dogodila se greska: {ex}! ')
